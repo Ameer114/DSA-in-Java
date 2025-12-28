07b6d0b442396f767650fb2c5880d755052f8cf7
Ameer Nagarasi <124485878+Ameer114@users.noreply.github.com>
2025-12-28 13:31:28 +0530

    LC_334_Increasing_Triplet

---
+++ Array/LC_334_Increasing_Triplet.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int first=Integer.MAX_VALUE;
+        int second=Integer.MAX_VALUE;
+        for(int i:nums){
+            if(first>=i)
+            first=i;
+            else if(second>i)
+            second=i;
+            else
+            return true;
+        }
+        return false;
+    }
+}
