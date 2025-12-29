42c7f57f6e69403bc5c2e763d283f261c875f136
Ameer Nagarasi <124485878+Ameer114@users.noreply.github.com>
2025-12-29 19:00:40 +0530

    Implement maxAscendingSum method in Solution class

---
+++ Array/lc1800_max_subArray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int sum=nums[0];
+        int max=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>nums[i-1]){
+                sum+=nums[i];
+            }
+            else{
+
+               if(sum>max)max=sum;
+               sum=nums[i];
+
+            }
+        }
+        if(sum>max)max=sum;
+
+        return max;
+    }
+}
